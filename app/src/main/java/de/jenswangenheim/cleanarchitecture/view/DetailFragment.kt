92088efa4510a053a26a6cda4529d954a79d6662
wangenheim.jens@gmail.com <wangenheim.jens@gmail.com>
2020-07-26 17:38:07 +0200

    Added navigation fundamentals

---
+++ app/src/main/java/de/jenswangenheim/cleanarchitecture/view/DetailFragment.kt
@@ -0,0 +1,30 @@
+package de.jenswangenheim.cleanarchitecture.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import de.jenswangenheim.cleanarchitecture.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+class DetailFragment : Fragment() {
+
+    private var dogUuid = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
+        }
+    }
+}
